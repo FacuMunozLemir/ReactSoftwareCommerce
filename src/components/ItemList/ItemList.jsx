c4c2ee51c20d554db486047ae582a4dd4b922ad5
ExpoJuy <crystaldesarrollo@gmail.com>
2022-09-27 09:03:46 -0300

    Se corrigió el componente donde se hacía la consulta a la API, ahora dicha consulta es´ta en ItemListContainer

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -2,18 +2,9 @@ import getItems from "../../services/mockAPI";
 import { useEffect, useState } from "react";
 import Item from "../Item/Item";
 
-export default function ItemList(props) {
-  let [data, setData] = useState([]);
-
-  useEffect(() => {
-    getItems().then((respuestaDatos) => {
-      setData(respuestaDatos);
-      console.log("ejecutamos getItems");
-    });
-  }, []);
-
-  return data.map((item) => {
-    console.log("item");
+export default function ItemList({ itemData }) {
+  return itemData.map((item) => {
+    console.log(item);
     return (
       <Item
         key={item.id}
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,5 +1,7 @@
 import "./itemListContainer.css";
 import ItemList from "../ItemList/ItemList";
+import { useState, useEffect } from "react";
+import getItems from "../../services/mockAPI";
 
 //1. Obtener los datos de nuestra mock api
 // estado
@@ -9,11 +11,20 @@ import ItemList from "../ItemList/ItemList";
 // ...map
 
 function ItemListContainer(props) {
+  let [data, setData] = useState([]);
+
+  useEffect(() => {
+    getItems().then((respuestaDatos) => {
+      setData(respuestaDatos);
+      console.log("ejecutamos getItems");
+    });
+  }, [data]);
+
   return (
     <div className="container">
       <h1>{props.greeting}</h1>
       <div className="itemsContainer">
-        <ItemList />
+        <ItemList itemData={data} />
       </div>
     </div>
   );
